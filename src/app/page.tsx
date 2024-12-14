9e870524fb9744b121283fa7e34ba1468dc2945e
rawadhossain <rawad.hossain00@gmail.com>
2024-12-14 14:25:11 +0600

    home page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import Image from 'next/image';
 import Navbar from './components/Navbar';
+import { div, main } from 'framer-motion/client';
+import HeroSection from './components/HeroSection';
 
 export default function Home() {
-    return <></>;
+    return (
+        <div>
+            <HeroSection />
+        </div>
+    );
 }
