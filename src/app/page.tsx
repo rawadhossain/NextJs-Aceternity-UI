0db9ed2799c29332956a0aeff2fa820c1b85b3e5
rawadhossain <rawad.hossain00@gmail.com>
2024-12-14 23:56:18 +0600

    background fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,17 @@ import { FeaturedCourses } from './components/FeaturedCourses';
 import { WhyChoseUs } from './components/WhyChoseUs';
 import MusicSchoolTestimonials from './components/TestimonialCards';
 import UpcomingWebinars from './components/UpcomingWebinars';
+import Instructors from './components/Instructors';
 
 export default function Home() {
     return (
-        <div>
+        <div className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
             <HeroSection />
             <FeaturedCourses />
             <WhyChoseUs />
             <MusicSchoolTestimonials />
             <UpcomingWebinars />
+            <Instructors />
         </div>
     );
 }
